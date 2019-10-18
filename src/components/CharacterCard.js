fc5dde725d98bb9066739376c63996e05fd2365c
Kevin Schoell <schoell411@gmail.com>
2019-10-18 10:46:32 -0500

    completed character list.js

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function CharacterCard() {
-  return <span>todo: character</span>;
+  return (
+    <div>
+      <h2>Character Name</h2>
+      <p>image
+        character Name
+          gender 
+          species
+      </p>
+    </div>
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
+    axios.get(`https://rickandmortyapi.com/api/character/`)
+    .then(response => {
+      console.log(response.data);
+      setCharacters(response.data);
+    })
+    .catch(error => {
+      console.log("The data was not returned", error);
+    });
     // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    //  Important: verify the 2nd `useEffect` parameter: the dependencies array!
   }, []);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <div>
+      characters.map(character => {
+        return (
+          <CharacterCard
+            key={character.id}
+            picture={character.image}
+            name={character.name}
+            gender={character.gender}
+            species={character.species}
+          />
+        )
+      })
+    </div>
   );
 }
